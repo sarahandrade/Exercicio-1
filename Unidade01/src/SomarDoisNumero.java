e012c7b73ed112f7a884bd7bb11ac0e4758e0271
sarahandrade <89746332+sarahandrade@users.noreply.github.com>
2021-08-29 15:46:46 -0300

    Add files via upload

---
+++ Unidade01/src/SomarDoisNumero.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class SomarDoisNumero {
+	public static Scanner sc = new Scanner(System.in);
+	
+	@Override
+	public String toString() {
+		return "SomarDoisNumero []";
+	}
+
+	public static Scanner getSc() {
+		return sc;
+	}
+
+	public static void setSc(Scanner sc) {
+		SomarDoisNumero.sc = sc;
+	}
+
+	public static void main (String args[]){
+		//Declaracao de variaveis
+		int num1, num2, soma;
+		//Leituras
+		System.out.println("Digite um número");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro número");
+		num2 = sc.nextInt();
+		//Somar
+		soma = num1 + num2;
+		//Mostrar na tela
+		System.out.println("Soma:" + soma);
+		}
+
+}
+ 
